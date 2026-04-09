175d8a1c5ee35cf8602aeeeb7862fd3242ab073f
AdAstra1157 <almaskhan@ad-astrainc.com>
2026-04-09 07:52:19 +0500

    some fixes for the vervcell issues

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    unoptimized: false,
+  },
+};
+
+module.exports = nextConfig;
